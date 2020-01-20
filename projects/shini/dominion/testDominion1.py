24c839bfc4cc9e1ec4100caa1118e588d541bc13
Christine Shi <52367357+snjchris@users.noreply.github.com>
2020-01-19 16:00:16 -0800

    Edited testDominion and testUtility files
    Added test scenarios to each testDominion file; refactored code to testUtility

---
+++ projects/shini/dominion/testDominion1.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 10:23 2020
+
+@author: NianJun Shi
+"""
+
+import Dominion
+import testUtility
+import random
+from collections import defaultdict
+
+#Get player names
+player_names = ["Annie","*Ben","*Carla"]
+
+#number of curses and victory cards
+if len(player_names)>2:
+    nV=12
+else:
+    nV=8
+nC = -10 + 10 * len(player_names)
+
+#Get box
+box = testUtility.generateBoxes(nV)
+
+#Get supply_order
+supply_order = testUtility.generateSupplyOrd()
+
+#TEST SCENARIO: Change supply order displayed in game
+supply_order = {5:['Estate', 'Cellar', 'Chapel', 'Moat']}
+
+#Pick 10 cards from box to be in the supply.
+boxlist = [k for k in box]
+random.shuffle(boxlist)
+random10 = boxlist[:10]
+supply = defaultdict(list,[(k,box[k]) for k in random10])
+
+#The supply always has these cards
+supply = testUtility.populateSupply(supply, nV, nC, player_names)
+
+#initialize the trash
+trash = []
+
+#Initialize players
+players = testUtility.generatePlayers(player_names)
+
+#Play the game
+turn  = 0
+while not Dominion.gameover(supply):
+    turn += 1    
+    print("\r")    
+    for value in supply_order:
+        print (value)
+        for stack in supply_order[value]:
+            if stack in supply:
+                print (stack, len(supply[stack]))
+    print("\r")
+    for player in players:
+        print (player.name,player.calcpoints())
+    print ("\rStart of turn " + str(turn))    
+    for player in players:
+        if not Dominion.gameover(supply):
+            print("\r")
+            player.turn(players,supply,trash)
+            
+
+#Final score
+dcs=Dominion.cardsummaries(players)
+vp=dcs.loc['VICTORY POINTS']
+vpmax=vp.max()
+winners=[]
+for i in vp.index:
+    if vp.loc[i]==vpmax:
+        winners.append(i)
+if len(winners)>1:
+    winstring= ' and '.join(winners) + ' win!'
+else:
+    winstring = ' '.join([winners[0],'wins!'])
+
+print("\nGAME OVER!!!\n"+winstring+"\n")
+print(dcs)
